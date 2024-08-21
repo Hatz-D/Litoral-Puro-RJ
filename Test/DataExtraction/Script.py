325f03889fb3336653174452c448b316453555ff
Hatz-D <diogo@IWX1274256-QrP5.china.huawei.com>
2024-08-21 14:54:26 -0300

    Initial configurations

---
+++ Test/DataExtraction/Script.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from bs4 import BeautifulSoup as bs
+import requests
+
+def main():
+    url = "https://praialimpa.net/"
+    page = requests.get(url)
+
+    df = pd.DataFrame({"Praia":[], "Local":[], "Qualidade":[], "Municipio":[], "Data":[]})
+    soup = bs(page.content, "html.parser")
+    sections = soup.find_all("section")
+
+    for section in sections:        
+        status = section.find_all(class_=["propria","impropria","amostragem-nao-realizada"])
+        praias = section.find_all(class_="name")
+        locais = section.find_all(class_="location")
+        municipio = section.find("h1").get_text()
+        lastupdate = section.find(class_="last-update").get_text()
+
+        for i in range(0, len(praias)):
+            new_line = {"Praia":praias[i].get_text(strip=True), "Local":locais[i].get_text(strip=True), "Qualidade":status[i].get_text(strip=True), "Municipio":municipio, "Data":lastupdate}
+            df = df._append(new_line, ignore_index=True)
+
+    df.to_json("./data.json")
+
+
+main()
+
