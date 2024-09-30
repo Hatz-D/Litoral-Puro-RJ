c2f3d0f4a3db9cf471c7d6a224cc61f3d996cab5
Hatz-D <10402406@mackenzista.com.br>
2024-09-30 18:55:34 -0300

    testes não mocados

--- Test/DataExtraction/Script.py
+++ Test/DataExtraction/Script.py
@@ -1,17 +1,12 @@
 import pandas as pd
 from bs4 import BeautifulSoup as bs
 import requests
-
-def jenkins():
-    return 1
-
-def jenkins2():
-    return 2
-
-def test():
-    assert jenkins() == 1 and jenkins2() == 2
+import pytest
+import json
+from unidecode import unidecode
 
 
+@pytest.fixture
 def main():
     url = "https://praialimpa.net/"
     page = requests.get(url)
@@ -24,15 +19,39 @@ def main():
         status = section.find_all(class_=["propria","impropria","amostragem-nao-realizada"])
         praias = section.find_all(class_="name")
         locais = section.find_all(class_="location")
-        municipio = section.find("h1").get_text()
-        lastupdate = section.find(class_="last-update").get_text()
+        municipio = unidecode(section.find("h1").get_text())
+        lastupdate = unidecode(section.find(class_="last-update").get_text())
 
         for i in range(0, len(praias)):
-            new_line = {"Praia":praias[i].get_text(strip=True), "Local":locais[i].get_text(strip=True), "Qualidade":status[i].get_text(strip=True), "Municipio":municipio, "Data":lastupdate}
+            new_line = {"Praia":unidecode(praias[i].get_text(strip=True)), "Local":unidecode(locais[i].get_text(strip=True)), "Qualidade":unidecode(status[i].get_text(strip=True)), "Municipio":municipio, "Data":lastupdate}
             df = df._append(new_line, ignore_index=True)
 
     df.to_json("./data.json")
+    return df.to_json()
 
 
-main()
+def test_estrutura(main):
+    assert "Praia" in main
+    assert "Local" in main
+    assert "Qualidade" in main
+    assert "Municipio" in main
+    assert "Data" in main
+
+
+def test_tamanho(main):
+    main = json.loads(main)
+    assert len(main["Praia"]) == 266
+    assert len(main["Local"]) == 266
+    assert len(main["Qualidade"]) == 266
+    assert len(main["Municipio"]) == 266
+    assert len(main["Data"]) == 266
+
+
+def test_status(main):
+    validos = {"Propria", "Impropria", "n/a"}
+    main = json.loads(main)
+    for i in range(len(main)):
+        status = main["Qualidade"][str(i)]
+        assert status in validos
+
 
