6148483f7c8c5ab5d554093047a2cf6d5448638e
Diogo Hatz <124005927+Hatz-D@users.noreply.github.com>
2024-10-08 14:23:05 -0300

    Create index.js

---
+++ Test/Website/index.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetchData();
+});
+
+function fetchData() {
+    fetch('http://dioguitoposeidon.com.br:8000/api/data')
+    .then(response => response.json())
+    .then(data => {
+        displayData(data);
+    })
+    .catch(error => {
+        console.error('Erro ao buscar os dados!:', error);
+    });
+}
+
+function displayData(data) {
+    const dataContainer = document.getElementById("data-container");
+
+    dataContainer.innerHTML = '';
+
+    data.forEach(item => {
+        const dataItem = document.createElement('div');
+        dataItem.classList.add('data-item');
+        dataItem.textContent = `Praia: ${item.Praia}, Local: ${item.Local}, Qualidade: ${item.Qualidade}, Municipio: ${item.Municipio}, Data: ${item.Data}`;
+        dataContainer.appendChild(dataItem);
+    })
+}
