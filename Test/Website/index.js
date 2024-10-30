2901a713131ec90f2131c141e38eaca182bd1aa8
Diogo Hatz <124005927+Hatz-D@users.noreply.github.com>
2024-10-30 07:34:33 -0300

    Add files via upload

--- Test/Website/index.js
+++ Test/Website/index.js
@@ -22,55 +22,90 @@ function initMap() {
       mapId: "MAP"
   });
 
-  loadMarkers(map);
+  fetchData(map);
 }
 
-async function loadMarkers(map) {
+async function loadMarkers(map, data) {
   const {AdvancedMarkerElement} = await google.maps.importLibrary("marker");
 
-  const greenPin = new google.maps.marker.PinElement({
-    background: "#32CD32",
-    borderColor: "#008000",
-    glyphColor: "#008000",
-  });
+  if (typeof data === 'string') {
+    try {
+        data = JSON.parse(data);
+    } catch (e) {
+        dataContainer.textContent = 'Erro ao processar os dados: formato inválido de JSON.';
+        return;
+    }
+}
 
-  const arpoadorInfo = new google.maps.InfoWindow({
-    content: "<h1>Arpoador</h1><p>Canto esquerdo da praia</p>",
-    ariaLabel: "Arpoador",
-  });
+  const coordenadas = JSON.parse("[[-22.98855, -43.193981], [-23.011334, -43.366277], [-23.011135, -43.335107], [-23.015383, -43.303935], [-23.016103, -43.297668], [-23.068052, -43.567816], [-22.945311, -43.18059], [-22.943248, -43.179182], [-22.985577, -43.188452], [-22.981945, -43.189236], [-22.973637, -43.184518], [-22.969771, -43.180013], [-22.988399, -43.189382], [-22.935312, -43.171021], [-22.928934, -43.170858], [-23.048297, -43.514021], [-23.048404, -43.52347], [-22.987135, -43.214321], [-22.987062, -43.209284], [-22.987171, -43.205046], [-23.014608, -43.290439], [-22.988491, -43.226842], [-22.987658, -43.222237], [-22.987056, -43.217069], [-22.963631, -43.167763], [-23.000519, -43.269344], [-23.031927, -43.472914], [-23.031481, -43.477817], [-23.04091, -43.505312], [-23.02697, -43.460558], [-23.023483, -43.44962], [-23.013437, -43.394248], [-23.016061, -43.412101], [-23.030398, -43.468566], [-22.99984, -43.264822], [-22.998808, -43.254686], [-22.94778, -43.163434], [-22.955202, -43.16463], [-22.992147, -43.232502]]")
 
-  const arpoadorMarker = new google.maps.marker.AdvancedMarkerElement({
-    map: map,
-    position: { lat: -22.988550, lng: -43.193981 },
-    title: "Arpoador",
-    content: greenPin.element,
-  });
+  coordenadas.forEach((coordenada, index) => {
+    const latitude = coordenada[0];
+    const longitude = coordenada[1];
 
-  arpoadorMarker.addListener("gmp-click", () => {
-    arpoadorInfo.open({
-      anchor: arpoadorMarker,
-      map,
+    const nomePraia = data.Praia[index];
+    const localPraia = data.Local[index];
+    const qualidade = data.Qualidade[index];
+    const municipio = data.Municipio[index];
+    const dataAtualizacao = data.Data[index];
+
+    const infoWindowContent = `
+      <h1>${nomePraia}</h1>
+      <p>${localPraia}</p>
+      <p><b>Qualidade da água:</b> ${qualidade}</p>
+      <p><b>Município:</b> ${municipio}</p>
+      <p><b>Última atualização:</b> ${dataAtualizacao}</p>
+    `;
+
+    const greenPin = new google.maps.marker.PinElement({
+      background: "#32CD32",
+      borderColor: "#008000",
+      glyphColor: "#008000",
+    });
+  
+    const greyPin = new google.maps.marker.PinElement({
+      background: "#bdbebd",
+      borderColor: "#808080",
+      glyphColor: "#808080",
+    });
+
+    const infoWindow = new google.maps.InfoWindow({
+      content: infoWindowContent,
+      ariaLabel: nomePraia,
+    });
+
+    const marker = new google.maps.marker.AdvancedMarkerElement({
+      map: map,
+      position: { lat: latitude, lng: longitude },
+      title: `${nomePraia} - ${localPraia}`,
+    });
+
+    if(qualidade.trim() === 'Propria') {
+      marker.content = greenPin.element;
+    }
+
+    else if(qualidade.trim() === 'n/a') {
+      marker.content = greyPin.element;
+    }
+
+    marker.addListener("gmp-click", () => {
+      infoWindow.open({
+        anchor: marker,
+        map,
+      });
     });
   });
+}
 
-  const tijucaAyrtonInfo = new google.maps.InfoWindow({
-    content: "<h1>Barra da Tijuca</h1><p>Em frente a Avenida Ayrton Senna</p>",
-    ariaLabel: "Barra da Tijuca Ayrton Senna",
+function fetchData(map) {
+  fetch('http://dioguitoposeidon.com.br:8000/api/data')
+  .then(response => response.json())
+  .then(data => {
+      loadMarkers(map, data);
+  })
+  .catch(error => {
+      console.error('Erro ao buscar os dados!:', error);
   });
-
-  const tijucaAyrtonMarker = new google.maps.marker.AdvancedMarkerElement({
-    map: map,
-    position: { lat: -23.011334, lng: -43.366277 },
-    title: "Barra da Tijuca Ayrton Senna",
-  });
-
-  tijucaAyrtonMarker.addListener("gmp-click", () => {
-    tijucaAyrtonInfo.open({
-      anchor: tijucaAyrtonMarker,
-      map,
-    });
-  });
-
 }
 
 // Carrega o Google Maps quando o DOM estiver pronto
