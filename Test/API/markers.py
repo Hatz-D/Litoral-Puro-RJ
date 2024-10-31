3628ceb7f7b43be61f2291b068b0c41007815a38
Diogo Hatz <124005927+Hatz-D@users.noreply.github.com>
2024-10-31 17:28:22 -0300

    Add files via upload

---
+++ Test/API/markers.py
@@ -0,0 +1,338 @@
+import json
+
+array = []
+
+# Rio de Janeiro
+array.append([-22.988550, -43.193981])
+array.append([-23.011334, -43.366277])
+array.append([-23.011135, -43.335107])
+array.append([-23.015383, -43.303935])
+array.append([-23.016103, -43.297668])
+array.append([-23.068052, -43.567816])
+array.append([-22.945311, -43.180590])
+array.append([-22.943248, -43.179182])
+array.append([-22.985577, -43.188452])
+array.append([-22.981945, -43.189236])
+array.append([-22.973637, -43.184518])
+array.append([-22.969771, -43.180013])
+array.append([-22.988399, -43.189382])
+array.append([-22.935312, -43.171021])
+array.append([-22.928934, -43.170858])
+array.append([-23.048297, -43.514021])
+array.append([-23.048404, -43.523470])
+array.append([-22.987135, -43.214321])
+array.append([-22.987062, -43.209284])
+array.append([-22.987171, -43.205046])
+array.append([-23.014608, -43.290439])
+array.append([-22.988491, -43.226842])
+array.append([-22.987658, -43.222237])
+array.append([-22.987056, -43.217069])
+array.append([-22.963631, -43.167763])
+array.append([-23.000519, -43.269344])
+array.append([-23.031927, -43.472914])
+array.append([-23.031481, -43.477817])
+array.append([-23.040910, -43.505312])
+array.append([-23.026970, -43.460558])
+array.append([-23.023483, -43.449620])
+array.append([-23.013437, -43.394248])
+array.append([-23.016061, -43.412101])
+array.append([-23.030398, -43.468566])
+array.append([-22.999840, -43.264822])
+array.append([-22.998808, -43.254686])
+array.append([-22.947780, -43.163434])
+array.append([-22.955202, -43.164630])
+array.append([-22.992147, -43.232502])
+
+# Niterói
+
+array.append([-22.927626, -43.122820])
+array.append([-22.907944, -43.128484])
+array.append([-22.961400, -43.057850])
+array.append([-22.967289, -43.047984])
+array.append([-22.917774, -43.095007])
+array.append([-22.931175, -43.098026])
+array.append([-22.935882, -43.105497])
+array.append([-22.929612, -43.122534])
+array.append([-22.905242, -43.124361])
+array.append([-22.905536, -43.121097])
+array.append([-22.902013, -43.136131])
+array.append([-22.904368, -43.117677])
+array.append([-22.904528, -43.116037])
+array.append([-22.908394, -43.111872])
+array.append([-22.911031, -43.110118])
+array.append([-22.974364, -43.035325])
+array.append([-22.975522, -43.029958])
+array.append([-22.973183, -43.045769])
+array.append([-22.969088, -43.046698])
+array.append([-22.934583, -43.112956])
+array.append([-22.933528, -43.113526])
+array.append([-22.953037, -43.098046])
+array.append([-22.953771, -43.095353])
+array.append([-22.954490, -43.087722])
+array.append([-22.956060, -43.075214])
+array.append([-22.915697, -43.095428])
+array.append([-22.918544, -43.094746])
+array.append([-22.921136, -43.094830])
+
+# Paraty
+
+array.append([-22.939521, -42.036254])
+array.append([-23.239710, -44.634063])
+array.append([-23.206294, -44.717749])
+array.append([-23.045340, -44.595303])
+array.append([-23.047583, -44.570481])
+array.append([-23.178219, -44.710920])
+array.append([-23.354210, -44.724678])
+array.append([-23.347207, -44.719394])
+
+#Conceição de Jacareí  Rua Wanderley do Nascimento Simões Maricá
+
+array.append([-22.956946, -42.698192])
+array.append([-22.969408, -42.979144])
+array.append([-22.970336, -42.961642])
+array.append([-22.957072, -42.742136])
+
+# Conceição de Jacarei
+
+array.append([-23.032387, -44.162307])
+array.append([-23.038236, -44.200363])
+array.append([-23.035568, -44.148684])
+
+# Região da Costa Verde (Mangaratiba e Itaguaí)
+
+array.append([-22.961057, -44.042251])
+array.append([-22.943668, -44.044834])
+array.append([-22.945296, -44.042628])
+array.append([-22.962049, -44.025264])
+array.append([-22.963264, -44.028571])
+array.append([-22.994488, -44.094545])
+array.append([-22.997387, -44.096900])
+array.append([-22.926723, -43.950770])
+array.append([-22.929941, -43.941197])
+array.append([-22.926901, -43.948743])
+array.append([-22.928064, -43.945177])
+array.append([-22.930185, -43.915038])
+array.append([-22.930238, -43.912512])
+array.append([-22.930856, -43.910264])
+array.append([-22.905819, -43.871789])
+array.append([-22.904731, -43.868635])
+array.append([-22.906436, -43.873016])
+
+# Ilha do Governador e Ramos
+
+array.append([-22.829320, -43.235339])
+array.append([-22.820617, -43.226804])
+array.append([-22.816740, -43.209495])
+array.append([-22.819236, -43.200608])
+array.append([-22.818766, -43.197266])
+array.append([-22.822930, -43.164975])
+array.append([-22.821137, -43.171672])
+array.append([-22.806525, -43.177497])
+array.append([-22.809161, -43.177536])
+array.append([-22.800387, -43.174975])
+array.append([-22.793320, -43.169559])
+array.append([-22.784102, -43.184847])
+array.append([-22.838890, -43.249418])
+
+# Paquetá
+
+array.append([-22.767554, -43.105962])
+array.append([-22.765730, -43.103636])
+array.append([-22.763346, -43.106875])
+array.append([-22.757137, -43.108395])
+array.append([-22.749813, -43.105492])
+array.append([-22.761435, -43.110922])
+
+# Saquarema
+
+array.append([-22.936003, -42.465895])
+array.append([-22.936125, -42.481427])
+array.append([-22.934263, -42.498887])
+array.append([-22.933265, -42.515416])
+array.append([-22.933648, -42.505871])
+array.append([-23.011644, -43.422579])
+array.append([-22.922658, -42.528747])
+array.append([-22.935717, -42.491621])
+array.append([-22.912419, -42.482623])
+
+# Araruama
+
+array.append([-22.915579, -42.311165])
+array.append([-22.919727, -42.307528])
+array.append([-22.889149, -42.348853])
+array.append([-22.891371, -42.366502])
+array.append([-22.894870, -42.363534])
+array.append([-22.889924, -42.369276])
+array.append([-22.874149, -42.334646])
+array.append([-22.873944, -42.333681])
+array.append([-22.874666, -42.329025])
+array.append([-22.874276, -42.330016])
+array.append([-22.874939, -42.328648])
+array.append([-23.013037, -43.295622])
+array.append([-22.869040, -42.303222])
+array.append([-22.867608, -42.295152])
+array.append([-22.867038, -42.298457])
+array.append([-22.867277, -42.296263])
+array.append([-22.858509, -42.248150])
+array.append([-22.859795, -42.254201])
+array.append([-22.863690, -42.257398])
+array.append([-22.870823, -42.283913])
+array.append([-22.879380, -42.272853])
+
+# Iguaba Grande e São Pedro d'Aldeia
+
+array.append([-22.847088, -42.228892])
+array.append([-22.840413, -42.222594])
+array.append([-22.839687, -42.217283])
+array.append([-22.841336, -42.210986])
+array.append([-22.841709, -42.207847])
+array.append([-22.848401, -42.162936])
+array.append([-22.830203, -42.109008])
+array.append([-22.835290, -42.104919])
+array.append([-22.848315, -42.103752])
+array.append([-22.860396, -42.109946])
+array.append([-22.869838, -42.115928])
+array.append([-22.840212, -42.136188])
+array.append([-22.865478, -42.111493])
+array.append([-22.850629, -42.094127])
+
+# Cabo Frio
+
+array.append([-22.873343, -42.052873])
+array.append([-22.882436, -42.010396])
+array.append([-22.883481, -42.013970])
+array.append([-22.885702, -42.016940])
+array.append([-22.875513, -42.007680])
+array.append([-22.869378, -41.983202])
+array.append([-22.860122, -41.986345])
+array.append([-22.873101, -42.045330])
+array.append([-22.919975, -42.037184])
+
+# Arraial do Cabo
+
+array.append([-22.951283, -42.107006])
+array.append([-22.941209, -42.035374])
+array.append([-22.967206, -42.035102])
+array.append([-22.975261, -42.021316])
+array.append([-22.969919, -42.019173])
+array.append([-22.959166, -42.025080])
+array.append([-22.951255, -42.107333])
+
+# Búzios
+
+array.append([-22.727910, -41.977405])
+array.append([-22.743755, -41.951731])
+array.append([-22.800634, -41.930125])
+array.append([-22.771425, -41.915952])
+array.append([-22.779396, -41.911306])
+array.append([-22.755172, -41.891031])
+array.append([-22.750226, -41.882576])
+array.append([-22.745932, -41.881309])
+array.append([-22.741974, -41.881677])
+array.append([-22.742205, -41.875416])
+array.append([-22.754691, -41.872630])
+array.append([-22.761265, -41.875244])
+array.append([-22.769072, -41.887274])
+array.append([-22.757091, -41.904804])
+
+# Casimiro de Abreu e Unamar (Cabo Frio)
+
+array.append([-22.589583, -41.987848])
+array.append([-22.584806, -41.986869])
+array.append([-22.578951, -41.985048])
+array.append([-22.661937, -41.998284])
+array.append([-22.641077, -41.999713])
+array.append([-22.649139, -41.999364])
+array.append([-22.611461, -41.998137])
+array.append([-22.600732, -41.995984])
+
+# Rio das Ostras
+
+array.append([-22.528514, -41.941513])
+array.append([-22.533136, -41.960302])
+array.append([-22.528106, -41.946152])
+array.append([-22.530807, -41.953263])
+array.append([-22.531634, -41.940383])
+array.append([-22.533036, -41.937227])
+array.append([-22.536050, -41.936447])
+array.append([-22.529961, -41.925455])
+array.append([-22.521361, -41.918876])
+array.append([-22.516264, -41.916445])
+array.append([-22.450220, -41.855091])
+array.append([-22.506830, -41.912110])
+
+# Macaé
+
+array.append([-22.400619, -41.791556])
+array.append([-22.400651, -41.791698])
+array.append([-22.403974, -41.794723])
+array.append([-22.385128, -41.771270])
+array.append([-22.381259, -41.770210])
+array.append([-22.376118, -41.771613])
+array.append([-22.367224, -41.774577])
+array.append([-22.413764, -41.813319])
+array.append([-22.348230, -41.757479])
+array.append([-22.331926, -41.730577])
+array.append([-22.308878, -41.702562])
+array.append([-22.416348, -41.818303])
+
+# Campos
+
+array.append([-22.033773, -41.029510])
+array.append([-22.037739, -41.037913])
+array.append([-22.042830, -41.047804])
+array.append([-22.044950, -41.052440])
+array.append([-22.052320, -41.067220])
+array.append([-22.061822, -41.084541])
+array.append([-22.070026, -41.098857])
+array.append([-22.075721, -41.108982])
+
+# São João da Barra
+
+array.append([-21.699878, -41.023951])
+array.append([-21.707536, -41.024219])
+array.append([-21.688758, -41.022984])
+array.append([-21.647252, -41.016166])
+array.append([-21.637116, -41.014795])
+array.append([-21.622037, -41.012847])
+array.append([-21.699875, -41.029099])
+array.append([-21.701280, -41.030305])
+array.append([-21.708328, -41.031974])
+
+# São Francisco de Itabapoana
+
+array.append([-21.456502, -41.034515])
+array.append([-21.477786, -41.056491])
+array.append([-21.491710, -41.065905])
+array.append([-21.534353, -41.070087])
+array.append([-21.516293, -41.072651])
+array.append([-21.500610, -41.070070])
+array.append([-21.565622, -41.057945])
+
+# Angra dos Reis
+
+array.append([-23.020476, -44.332415])
+array.append([-23.022157, -44.339713])
+array.append([-23.022669, -44.341663])
+array.append([-23.003094, -44.239361])
+array.append([-23.006396, -44.480213])
+array.append([-23.026820, -44.518549])
+array.append([-23.023174, -44.333855])
+array.append([-23.015446, -44.322462])
+array.append([-22.983339, -44.315353])
+array.append([-22.966206, -44.435625])
+array.append([-23.033825, -44.172615])
+array.append([-22.993222, -44.326020])
+array.append([-23.010128, -44.233782])
+array.append([-23.003409, -44.263786])
+array.append([-22.953207, -44.385738])
+array.append([-23.030828, -44.235743])
+array.append([-23.020345, -44.357689])
+array.append([-23.010324, -44.444927])
+array.append([-23.024699, -44.498668])
+
+
+json_string = json.dumps(array)
+
+with open("coordinates.json", "w") as arquivo:
+    json.dump(json_string, arquivo)
