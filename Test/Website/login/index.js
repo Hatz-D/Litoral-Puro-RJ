f87deb26589d41c87a3ea2019f96f2912f795e07
Diogo Hatz <124005927+Hatz-D@users.noreply.github.com>
2025-02-27 14:02:50 -0300

    Update index.js

--- Test/Website/login/index.js
+++ Test/Website/login/index.js
@@ -1,14 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
   const loginForm = document.querySelector("#loginForm");
-  
+
   loginForm.addEventListener("submit", async function(event) {
     event.preventDefault();
-    
+
     const email = document.querySelector("#email").value;
     const password = document.querySelector("#password").value;
-    
+
     try {
-      // Envia a requisição de login para a API
       const response = await fetch("http://dioguitoposeidon.com.br:8000/api/login", {
         method: "POST",
         headers: {
@@ -20,13 +19,11 @@ document.addEventListener("DOMContentLoaded", function() {
       if (response.ok) {
         const data = await response.json();
         console.log("Login bem-sucedido!", data);
-        
-        // Exibe o nome do usuário no frontend
-        const userName = data.user.name;
-        document.getElementById("user-info").style.display = "block"; // Exibe a área com o nome
-        document.getElementById("username").textContent = userName; // Define o nome no frontend
-        
-        // Redireciona para o dashboard
+
+        // Armazena o nome do usuário no localStorage
+        localStorage.setItem("userName", data.user.name);
+
+        // Redireciona para a página inicial (index.html)
         window.location.href = "/index.html";
       } else {
         const errorData = await response.json();
