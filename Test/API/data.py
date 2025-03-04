babbc44a4f2c248b4038d83af950f072322dfd8b
Diogo Hatz <124005927+Hatz-D@users.noreply.github.com>
2025-03-04 15:30:50 -0300

    Update data.py

--- Test/API/data.py
+++ Test/API/data.py
@@ -233,6 +233,7 @@ async def register(user: UserCreate):
             status_code=status.HTTP_201_CREATED
         )
     except ValueError as ve:
+        print(e)
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(ve))
     except Exception as e:
         print(e)
