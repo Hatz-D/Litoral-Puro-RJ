66d29e4f67eedb245f6deaf6fc6233005715f36e
Diogo Hatz <124005927+Hatz-D@users.noreply.github.com>
2025-03-04 22:11:04 -0300

    Update index.js

--- Test/Website/lista/index.js
+++ Test/Website/lista/index.js
@@ -1,3 +1,27 @@
+// Verifica se há um nome de usuário armazenado no localStorage
+const userName = localStorage.getItem("userName");
+const userNameElement = document.getElementById("user-name");
+const authButton = document.getElementById("auth-button");
+
+if (userName) {
+    userNameElement.textContent = `Bem-vindo, ${userName}!`;
+    authButton.textContent = "Logout";
+    authButton.onclick = () => {
+        localStorage.removeItem("userName");
+        localStorage.removeItem("userEmail");
+        userNameElement.textContent = '';
+        authButton.textContent = "Registrar/Login";
+        alert("Você foi deslogado com sucesso.");
+        window.location.href = "index.html";
+    };
+} else {
+    userNameElement.textContent = '';
+    authButton.textContent = "Registrar/Login";
+    authButton.onclick = () => {
+        window.location.href = "/login/index.html";
+    };
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     fetchData();
 });
