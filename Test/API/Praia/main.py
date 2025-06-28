f00523032472ee3542b90c48c7f0ed9d677300df
Diogo Hatz <124005927+Hatz-D@users.noreply.github.com>
2025-06-28 13:22:12 -0300

    Update main.py

--- Test/API/Praia/main.py
+++ Test/API/Praia/main.py
@@ -19,9 +19,6 @@ app = FastAPI()
 MAPS_API = os.getenv("MAPS_API")
 MONGO_URI = os.getenv("MONGO_URI")
 
-ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-ssl_context.load_cert_chain('/data/live/dioguitoposeidon.com.br/fullchain.pem', keyfile='/data/live/dioguitoposeidon.com.br/privkey.pem')
-
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 origins = [
